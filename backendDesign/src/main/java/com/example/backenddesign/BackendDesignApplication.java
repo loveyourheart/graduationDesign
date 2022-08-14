8c2afc326e3d8c4e832c927f1b26ea9afe5af98b
lizhihao <2048220108@qq.com>
2022-08-14 18:54:21 +0800

    four

---
+++ backendDesign/src/main/java/com/example/backenddesign/BackendDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.backenddesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDesignApplication.class, args);
+    }
+
+}
---
+++ backendDesign/src/test/java/com/example/backenddesign/BackendDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
